994cbe1c9c19c3a98c98a647ab59be34723f301e
RushilChalla <challa.rushil@gmail.com>
2025-08-31 17:49:49 -0400

    Add Main class with main method

---
+++ pantry/Inventory.java
@@ -0,0 +1,5 @@
+package pantry;
+
+public class Inventory {
+    
+}
---
+++ pantry/Main.java
@@ -0,0 +1,8 @@
+package pantry;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main is running!");
+
+    }
+}
